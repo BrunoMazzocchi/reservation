e256826d958eb7ce98c916dd474df5aaf5866696
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-02-05 21:15:31 -0600

    Initial commit ☕

---
+++ src/test/java/com/mazzocchi/reservation/ReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazzocchi.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
